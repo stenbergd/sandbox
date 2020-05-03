1f332c845e742ecc1027dd88021924cdd52d847d
Daniel Stenberg <daniel.stenberg87@outlook.com>
2020-05-03 17:27:59 +0200

    Added scipy odeint solver instead of backward Euler. Added argparse for command line arguments.

--- sir_model.py
+++ sir_model.py
@@ -25,34 +25,42 @@ Derivates in above differential equations discretized using backward Eulers meth
 Sample time must be small enough to provide stable results with sufficient accuracy
 '''
 
+import argparse
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.integrate import odeint
 
-days = 180
+
+def model_func(y, t, beta, gamma):
+	f = [ -beta * y[1] * y[0], beta * y[1] * y[0] - gamma * y[1], gamma * y[1] ]
+	return f
+
+parser = argparse.ArgumentParser(description='SIR Model epidemic simulator')
+
+parser.add_argument('--beta', '-b', action="store", dest="beta", type=float, default=2.3/14.0)
+parser.add_argument('--gamma', '-g', action="store", dest="gamma", type=float, default=1/14.0)
+parser.add_argument('--days', '-d', action="store", dest="days", type=int, default=180)
+
+args = parser.parse_args()
+
+beta = args.beta
+gamma = args.gamma
+days = args.days
+
 h = 0.1 # sample time
 t = np.linspace(0, days, (days+1)/h)
 
-Gamma = 1/14.0
-Beta = 2.3*Gamma
+r0 = beta/gamma
 
-s = np.zeros(int((days+1)/h))
-i = np.zeros(int((days+1)/h))
-r = np.zeros(int((days+1)/h))
+y0 = [ 0.999, 0.001, 0.0 ] # Initial susceptible, infectious and recovered population ratios
 
-s[0] = 0.99 # Initial susceptible population ratio
-i[0] = 0.01 # Initial infectious population ratio
-r[0] = 0.0 # Initial recovered population ratio
+y = odeint(model_func, y0, t, args=(beta, gamma))
 
-for k in range(0, len(t)-1):
-	s[k+1] = s[k] - Beta*i[k]*s[k]*h
-	i[k+1] = i[k] + Beta*i[k]*s[k]*h - Gamma*i[k]*h
-	r[k+1] = r[k] + Gamma*i[k]*h
-
-plt.plot(t, s, 'b', label='S(t) - Susceptible')
-plt.plot(t, i, 'r', label='I(t) - Infectious')
-plt.plot(t, r, 'g', label='R(t) - Recovered')
+plt.plot(t, y[:, 0], 'b', label='S(t) - Susceptible')
+plt.plot(t, y[:, 1], 'r', label='I(t) - Infectious')
+plt.plot(t, y[:, 2], 'g', label='R(t) - Recovered')
 plt.legend()
-plt.title('SIR Model - R0: {} (Beta: {}, Gamma: {})'.format(round(Beta/Gamma, 2), round(Beta, 2), round(Gamma, 2)))
+plt.title('SIR Model - R0: {} (Beta: {}, Gamma: {})'.format(round(r0, 2), round(beta, 2), round(gamma, 2)))
 plt.xlabel('Time [days]')
 plt.ylabel('Population ratio')
 plt.show()
